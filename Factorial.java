2867caf5e6ce9d28ba26979d9d2a63948becfbb1
Edward Korkollie <korkollieed@gmail.com>
2019-05-27 12:16:37 -0400

    Lab
    Basic com lab

--- Factorial.java
+++ Factorial.java
@@ -6,7 +6,13 @@ import java.math.BigInteger;
 public class Factorial {
 
     public BigInteger factorialOf(Integer value){
-        return null;
+        BigInteger fact = BigInteger.valueOf(1);
+        for (int i = 2; i <= value; i++)
+        {
+            fact = fact.multiply(BigInteger.valueOf(i));
+            
+        }
+        return fact;
     }
-
+    
 }
--- LargestInteger.java
+++ LargestInteger.java
@@ -4,10 +4,13 @@
 public class LargestInteger {
 
     public Integer findLargestNumberUsingConditional(Integer[] integers){
-        return null;
+        return(integers[0] > integers[1]) ? 
+        ((integers[0] > integers[2]) ? integers[0] : integers[2]): 
+        ((integers[2] > integers[1]) ? integers[2]: integers[1]); 
+        
     }
 
     public Integer findLargestNumberUsingMathMax(Integer[] integers){
-        return null;
+        return Math.max(Math.max(integers[0],integers[1]),integers[2]);
     }
 }
